760ce41edbae361cd914fdd1ee9ea112bc94fbba
Szymon Sakowicz <github@sakowi.cz>
2024-12-01 21:17:46 +0100

    Proper coverage files configuration

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  coveragePathIgnorePatterns: [
-    '/tests/test-doubles/',
-  ],
+  collectCoverageFrom: ['src/**/*.ts', 'app.ts'],
 };
